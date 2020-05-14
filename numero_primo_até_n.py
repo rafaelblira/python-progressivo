5c7aeff89e3a3e670885e72dc8eb27e70275aa87
Rafael Bruno <rafaelblira@yahoo.com.br>
2020-05-14 17:56:54 -0300

    mod 1

--- numero_primo_até_n.py
+++ numero_primo_até_n.py
@@ -1,5 +1,5 @@
 total = int(input('Digite o total da séria para ver os primos: '))
-for num in range(1, total):
+for num in range(2, total):
     prime = True
     for i in range(2,num):
         if (num%i==0):
